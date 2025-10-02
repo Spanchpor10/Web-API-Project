162451905faf20a969347e1e88a12064f6c3dd30
Sarthak Panchpor <spanchpor10@gmail.com>
2025-10-02 23:33:19 +0530

    Updated files

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Data.SqlClient;
+using WebApi_2.Models;
+using System.Data;
+using System.Configuration;
+
+namespace WebApi_2.Controllers
+{
+    public class MessagesController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            DataTable table= new DataTable();
+            string query = @"
+                           select MsgID,MsgBody from dbo.Messages";
+            using(var con=new SqlConnection(ConfigurationManager.ConnectionStrings["MessagesDBC"].ConnectionString))
+            using (var cmd=new SqlCommand(query, con))
+                using (var da=new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType= CommandType.Text;
+                da.Fill(table);
+
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+
+    }
+}
---
+++ Models/MessagesDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_2.Models
+{
+    public class MessagesDb
+    {
+
+        public int MsgID { get; set; }
+        public int MsgBody { get; set; }
+
+    }
+}
